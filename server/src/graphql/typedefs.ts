cad0b506cf92df94ee278328547fe0025da08a06
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-10-27 22:34:33 +0530

    added typedefs

--- server/src/graphql/typedefs.ts
+++ server/src/graphql/typedefs.ts
@@ -1,13 +1,92 @@
 import { gql } from "apollo-server";
 
+// here only the fields you want to query in future
 const typeDefs = gql`
-  type User {
-    id: ID!
-    firstName: String!
-    lastName: String!
-    email: String!
-    password: String!
+  scalar DateTime
+
+  enum Type {
+    GRATITUDE
+    EVENT
+    IDEA
+    LEARNING
   }
+
+  enum Priority {
+    VERY_HIGH
+    HIGH
+    MEDIUM
+    LOW
+    VERY_LOW
+  }
+
+  type User {
+    id: String!
+    name: String!
+    email: String!
+    plansForTomorrow: [TimeSlot]
+    journals: [Journal]
+    goals: [Goal]
+    habits: [Habit]
+    tasks: [Task]
+  }
+
+  type Tag {
+    id: String!
+    name: String!
+    description: String!
+    generic: Generic
+  }
+
+  type Generic {
+    id: String!
+    name: String!
+    description: String!
+    tags: [Tag]
+    task: [Task]
+    goal: [Goal]
+    habit: [Habit]
+    journal: [Journal]
+  }
+
+  type Journal {
+    id: String!
+    base: Generic!
+    type: Type!
+  }
+
+  type Task {
+    id: String!
+    base: Generic!
+    deadline: DateTime
+    priority: Priority
+    predictedHours: Float
+    TimeSlot: [TimeSlot]
+  }
+
+  type Goal {
+    id: String!
+    base: Generic!
+    why: String!
+    relevance: String!
+    predictedTimeline: [TimeSlot]
+  }
+
+  type Habit {
+    id: String!
+    base: Generic!
+    startDate: DateTime
+    trackRecord: [Boolean]
+    timeSlot: [TimeSlot]
+  }
+
+  type TimeSlot {
+    id: String!
+    start: DateTime!
+    end: DateTime!
+    name: String
+    task: Task
+  }
+
   type Query {
     allUsers: [User]
   }
